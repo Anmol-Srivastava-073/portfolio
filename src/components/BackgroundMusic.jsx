48f2a7cd5ef086331da773830334e8e41f033fd8
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-05-23 23:41:04 +0530

    Create BackgroundMusic.jsx

---
+++ src/components/BackgroundMusic.jsx
@@ -0,0 +1,9 @@
+function BackgroundMusic() {
+  return (
+    <audio autoPlay loop>
+      <source src="/music/interstellar.mp3" type="audio/mpeg" />
+    </audio>
+  )
+}
+
+export default BackgroundMusic
