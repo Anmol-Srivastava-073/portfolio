d59f479154372cec0aeb0e4f914effe045eaa9a3
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-05-23 20:09:42 +0530

    Create Education.jsx

---
+++ src/components/Education.jsx
@@ -0,0 +1,27 @@
+export default function Education() {
+  return (
+    <section className="max-w-7xl mx-auto px-6 py-24">
+      <h2 className="text-5xl font-bold text-center mb-16">
+        Education
+      </h2>
+
+      <div className="space-y-8">
+        <div className="bg-white/5 border border-white/10 rounded-3xl p-8">
+          <div className="flex gap-6 items-center">
+            <img src="/photos/manipallogo.png" className="w-20" />
+
+            <div>
+              <h3 className="text-2xl font-bold">
+                Manipal University Jaipur
+              </h3>
+
+              <p className="text-cyan-400 mt-2">
+                B.Tech CSE (2025 - 2029)
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
