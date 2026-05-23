1680ee2f930d3d54bbcec36cea2f32b567610f99
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-05-23 23:39:42 +0530

    Create Resume.jsx

---
+++ src/components/Resume.jsx
@@ -0,0 +1,21 @@
+function Resume() {
+  return (
+    <section className="py-24 px-6 text-center">
+      <h2 className="text-5xl font-bold mb-12">Resume</h2>
+
+      <img
+        src="/photos/anmolfinalres.png"
+        className="max-w-4xl mx-auto rounded-3xl shadow-2xl"
+      />
+
+      <a
+        href="/photos/AnmolResume.docx"
+        className="gradient px-8 py-4 rounded-full inline-block mt-10 text-black font-bold"
+      >
+        Download Resume
+      </a>
+    </section>
+  )
+}
+
+export default Resume
