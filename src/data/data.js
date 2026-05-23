d1262f75fc1c21f52857aa873bfefe728f95a3cf
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-05-23 20:05:35 +0530

    Create data.js with skills and projects arrays
    Added skills and projects data for portfolio.

---
+++ src/data/data.js
@@ -0,0 +1,41 @@
+export const skills = [
+  'Java',
+  'C++',
+  'Python',
+  'JavaScript',
+  'React',
+  'HTML',
+  'CSS',
+  'Tailwind',
+  'MySQL',
+  'Git',
+  'Flask',
+  'C'
+]
+
+export const projects = [
+  {
+    title: 'Verdian',
+    desc: 'Smart waste management system helping users dispose waste efficiently.',
+    image: '/photos/vx.png',
+    link: 'https://verdian-wastesystem.vercel.app/'
+  },
+  {
+    title: 'Veritas',
+    desc: 'AI healthcare chatbot for pre-consultation symptom guidance.',
+    image: '/photos/s4.png',
+    link: 'https://veritas-healthbot.vercel.app/'
+  },
+  {
+    title: 'Manipal UniNav',
+    desc: 'Interactive campus navigation platform for MUJ students.',
+    image: '/photos/uninav.png',
+    link: 'https://mujnavigator.netlify.app'
+  },
+  {
+    title: 'Spin Roulette',
+    desc: 'Probability based interactive roulette game.',
+    image: '/photos/s2.png',
+    link: 'https://spinroulette.netlify.app'
+  },
+]
