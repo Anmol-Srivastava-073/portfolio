e106a59edd469f1b3e7a4cac3f8e13d38e649601
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-05-24 00:44:37 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="text-center py-10 border-t border-white/10 text-gray-400">
+    <footer className="text-center py-10 border-t-4 border-ink font-handwritten text-2xl text-ink/80 font-bold bg-white">
       © 2026 Anmol Srivastava. All Rights Reserved.
     </footer>
   )
