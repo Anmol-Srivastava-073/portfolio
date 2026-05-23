b6a48cf7e5fceb1421a1836f547ca9871777e885
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-05-23 23:35:56 +0530

    Update and rename data.js to portfolioData.js

---
+++ src/data/portfolioData.js
@@ -0,0 +1,79 @@
+export const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Java',
+  'C++',
+  'MySQL',
+  'Python',
+  'Flask',
+  'Git',
+  'React',
+]
+
+export const education = [
+  {
+    title: 'Manipal University Jaipur',
+    location: 'Jaipur, Rajasthan',
+    desc: 'Bachelor of Technology in Computer Science (2025 - 2029)',
+    image: '/photos/manipallogo.png',
+  },
+  {
+    title: 'Maharshi Patanjali Vidya Mandir',
+    location: 'Prayagraj, Uttar Pradesh',
+    desc: 'Intermediate (2018 - 2025)',
+    image: '/photos/mpvmlogo.png',
+  },
+  {
+    title: 'Patanjali Rishikul',
+    location: 'Prayagraj, Uttar Pradesh',
+    desc: 'Junior and Middle School (2014 - 2018)',
+    image: '/photos/rishikullogo.jpeg',
+  },
+]
+
+export const experience = [
+  {
+    title: 'GirlScript Summer of Code 2026',
+    role: 'Contributor',
+    image: '/photos/gssoc.png',
+  },
+  {
+    title: 'AWS Cloud Clubs MUJ',
+    role: 'Technical Team Member',
+    image: '/photos/awstech.png',
+  },
+]
+
+export const projects = [
+  {
+    title: 'Manipal UniNav Map',
+    image: '/photos/uninav.png',
+    link: 'https://mujnavigator.netlify.app',
+    desc: 'Interactive university navigation platform.',
+  },
+  {
+    title: 'Veritas',
+    image: '/photos/s4.png',
+    link: 'https://veritas-healthbot.vercel.app/',
+    desc: 'Clinical AI chatbot before doctor consultation.',
+  },
+  {
+    title: 'Verdian',
+    image: '/photos/vx.png',
+    link: 'https://verdian-wastesystem.vercel.app/',
+    desc: 'Smart waste management platform.',
+  },
+  {
+    title: 'Spin Roulette',
+    image: '/photos/s2.png',
+    link: 'https://spinroulette.netlify.app',
+    desc: 'Fun roulette game project.',
+  },
+  {
+    title: 'Flipping A Coin',
+    image: '/photos/a1.png',
+    link: 'https://flippingacoin.netlify.app',
+    desc: 'Coin flipping simulator.',
+  },
+]
