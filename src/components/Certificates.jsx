0a23e8bcf87fffa0df4212658ed60b6c33111070
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-05-23 23:38:42 +0530

    Update Certificates.jsx

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,20 +1,29 @@
-export default function Certificates() {
-  return (
-    <section className="max-w-7xl mx-auto px-6 py-24">
-      <h2 className="text-5xl font-bold text-center mb-16">
-        Certificates
-      </h2>
+function Certificates() {
+  const certs = [
+    '/photos/jscert.png',
+    '/photos/genai.png',
+    '/photos/sqlcert.png',
+    '/photos/pythoncert.png',
+    '/photos/javacert.png',
+    '/photos/jvcert.png',
+    '/photos/jvoops.png',
+  ]
 
-      <div className="grid grid-cols-2 md:grid-cols-5 gap-5">
-        {['Java', 'Python', 'SQL', 'JavaScript', 'GenAI'].map((item) => (
-          <div
-            key={item}
-            className="bg-white/5 border border-white/10 rounded-2xl p-6 text-center"
-          >
-            {item}
-          </div>
+  return (
+    <section className="py-24 px-6 max-w-7xl mx-auto">
+      <h2 className="text-5xl font-bold text-center mb-16">Certificates</h2>
+
+      <div className="grid md:grid-cols-3 gap-8">
+        {certs.map((cert, index) => (
+          <img
+            key={index}
+            src={cert}
+            className="rounded-2xl hover:scale-105 transition duration-500 shadow-2xl"
+          />
         ))}
       </div>
     </section>
   )
 }
+
+export default Certificates
